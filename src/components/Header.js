5bb0242d4bd5fdcb924374d676dfceff9be9892c
James <jam3sstotomas@gmail.com>
2022-12-20 21:49:30 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+const Header = () => {
+    return (
+        <h1>Lucas' Schedule</h1>
+
+//Visual Schedule for Kids and Parents
+
+// Modal pop up asking for the childs name
+        //Display name in h1 "Lucas' Visual Schedule"
+
+//form input element to add task
+
+// on submit, take value of task and store it in Firebase
+
+//take object of tasks from Firebase, loop through and display tasks on page.
+        //i also want to incorporate displaying images (stored locally) to represent the task. if no image related, just display text. 
+        //if that idea does not work out, use task as search query in the unsplash api
+
+//when task is completed, user can click done and task will be removed from the page
+
+//Stretch goals
+//add option to add tasks for multiple children
+//add task based on Date and/or Time
+
+
+
+
+
+
+
+
+
+
+    )
+}
+
+export default Header
