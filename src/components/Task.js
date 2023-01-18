093428d8075e08941079f979e03db3d11e20eebf
James <jam3sstotomas@gmail.com>
2023-01-17 23:08:42 -0500

    created components based off wireframes, created onClick function for add task button

---
+++ src/components/Task.js
@@ -0,0 +1,9 @@
+const Task = () => {
+    return (
+        <section className="task">
+            <h2>Task Name</h2>
+        </section>
+    )
+}
+
+export default Task;
---
+++ src/components/TaskContainer.js
@@ -0,0 +1,9 @@
+const TaskContainer = () => {
+    return (
+        <section className="taskContainer">
+            <h2>Task Container</h2>
+        </section>
+    )
+}
+
+export default TaskContainer;
